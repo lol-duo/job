78ec0b1f8674c641ff672cba3cf3a550d72c49c8
imygnam <imygnam@gmail.com>
2024-02-08 18:41:33 +0900

    [FIX] add gc

--- setUserInfo/getUserSummonerIdByLeague/deploy.sh
+++ setUserInfo/getUserSummonerIdByLeague/deploy.sh
@@ -3,4 +3,4 @@
 REPOSITORY=/home/ec2-user/job1-1
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 chmod +x $JAR_NAME
-nohup /opt/jdk-17/bin/java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup /opt/jdk-17/bin/java -jar -XX:+UseParallelGC -Xms50m -Xmx50m -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9900 -Dcom.sun.management.jmxremote.rmi.port=9900 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Djava.rmi.server.hostname=43.203.53.58 $JAR_NAME > $REPOSITORY/nohup.out  2>&1 &
